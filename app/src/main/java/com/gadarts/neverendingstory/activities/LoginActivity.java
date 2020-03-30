a50baf3ef58b5e265c65031714534875d279a666
Gad Wissberg <gadw17@gmail.com>
2020-03-30 22:02:03 +0300

    Fixing bugs.

--- app/src/main/java/com/gadarts/neverendingstory/activities/LoginActivity.java
+++ app/src/main/java/com/gadarts/neverendingstory/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.gadarts.neverendingstory.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,7 @@ import com.gadarts.neverendingstory.HttpCallTask.RequestTypes;
 import com.gadarts.neverendingstory.R;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
@@ -19,20 +21,37 @@ public class LoginActivity extends FragmentActivity {
     private static final String LOGIN = ListActivity.HOST + "login";
     private static final String KEY_MAIL = "email";
     private static final String KEY_PASSWORD = "password";
+    public static final String PREFERENCES_LOGIN = "login";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCES_LOGIN, MODE_PRIVATE);
+        String mail = sharedPreferences.getString(KEY_MAIL, null);
+        String password = sharedPreferences.getString(KEY_PASSWORD, null);
+        if (Optional.ofNullable(mail).isPresent() && Optional.ofNullable(password).isPresent())
+            performLogin(mail, password);
+        else setLoginView();
+    }
+
+    private void setLoginView() {
         setContentView(R.layout.activity_login);
         Button loginButton = findViewById(R.id.button_login);
         EditText mailInput = findViewById(R.id.mail);
         EditText passwordInput = findViewById(R.id.password);
-        loginButton.setOnClickListener(button -> performLogin(mailInput, passwordInput));
+        loginButton.setOnClickListener(button -> performLogin(
+                String.valueOf(mailInput.getText()),
+                String.valueOf(passwordInput.getText())));
     }
 
-    private void performLogin(EditText mailInput, EditText passwordInput) {
+    private void performLogin(String mailInput, String passwordInput) {
         HttpCallTask task = new HttpCallTask(LOGIN, RequestTypes.POST,
                 (response) -> {
+                    SharedPreferences prefs = getSharedPreferences(PREFERENCES_LOGIN, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString(KEY_MAIL, String.valueOf(mailInput));
+                    editor.putString(KEY_PASSWORD, String.valueOf(passwordInput));
+                    editor.apply();
                     Intent intent = new Intent(this, ListActivity.class);
                     startActivity(intent);
                 },
@@ -44,10 +63,10 @@ public class LoginActivity extends FragmentActivity {
         task.execute();
     }
 
-    private HashMap<String, String> createLoginParameters(EditText mailInput, EditText passwordInput) {
+    private HashMap<String, String> createLoginParameters(String mailInput, String passwordInput) {
         HashMap<String, String> parameters = new HashMap<>();
-        parameters.put(KEY_MAIL, String.valueOf(mailInput.getText()));
-        parameters.put(KEY_PASSWORD, String.valueOf(passwordInput.getText()));
+        parameters.put(KEY_MAIL, String.valueOf(mailInput));
+        parameters.put(KEY_PASSWORD, String.valueOf(passwordInput));
         return parameters;
     }
 }
