630e3a857839b2470a51c2e1e3f293c7e6e90e91
Gad Wissberg <gadw17@gmail.com>
2020-05-23 23:23:05 +0300

    Cosmetics.

---
+++ app/src/test/java/com/gadarts/neverendingstory/http/AppRequestTest.java
@@ -0,0 +1,34 @@
+package com.gadarts.neverendingstory.http;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AppRequestTest {
+
+    private static final String URL = "http://127.0.0.1";
+    private static final String REQUEST_KEY = "key";
+    private static final String VALUE = "value";
+    @InjectMocks
+    private AppRequest request;
+
+    @Mock
+    private OnRequestResult result;
+
+    @Before
+    public void setUp() {
+        request = new AppRequest(URL, HttpCallTask.RequestType.GET, result);
+    }
+
+    @Test
+    public void addParameter() {
+        request.addParameter(REQUEST_KEY, VALUE);
+        assertEquals(request.getParameters().get(REQUEST_KEY), VALUE);
+    }
+}
