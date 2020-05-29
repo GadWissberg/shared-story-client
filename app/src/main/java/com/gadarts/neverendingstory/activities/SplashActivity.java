387de0907a7681529fa5bea40dc309d79f1cfb12
Gad Wissberg <gadw17@gmail.com>
2020-05-29 16:18:41 +0300

    Added splash screen.

---
+++ app/src/main/java/com/gadarts/neverendingstory/activities/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.gadarts.neverendingstory.activities;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+
+import com.gadarts.neverendingstory.R;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+
+    public static final int FADE_DURATION = 2000;
+    private static final long NEXT_ACTIVITY_DELAY = 4000;
+
+    @SuppressLint("SourceLockedOrientationActivity")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setFadeAnimation();
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+        changeToNextActivityWithDelay();
+    }
+
+    private void setFadeAnimation() {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+        alphaAnimation.setDuration(FADE_DURATION);
+        alphaAnimation.setFillAfter(true);
+        findViewById(R.id.activity_splash).startAnimation(alphaAnimation);
+    }
+
+    private void changeToNextActivityWithDelay() {
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, NEXT_ACTIVITY_DELAY);
+    }
+}
